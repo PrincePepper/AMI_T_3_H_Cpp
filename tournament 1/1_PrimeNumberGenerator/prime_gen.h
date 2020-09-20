b23c3ec30a8bc051a31833a8f8d2c065ad64f0d9
semyon <semyon3336@gmail.com>
2020-09-20 23:34:35 +1000

    Initial commit

---
+++ tournament 1/1_PrimeNumberGenerator/prime_gen.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class PrimeNumberGenerator {
+public:
+    explicit PrimeNumberGenerator(int start) { start_n = start; }
+
+    int GetNextPrime() {
+        while (check) {
+            check = isPrime(start_n);
+            start_n++;
+        }
+        check = true;
+        return start_n - 1;
+    }
+
+private:
+    int start_n = 0;
+    bool check = true;
+
+    static bool isPrime(int n) {
+        if (n > 1) {
+            for (int i = 2; i < n; i++)
+                if (n % i == 0) return true;
+            return false;
+        } else return true;
+    }
+};
+
