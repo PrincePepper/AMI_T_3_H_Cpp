d93d1c778332446e127c8b688d9922b0b96060b7
PrincePepper <semyon3336@gmail.com>
2020-10-11 22:45:17 +1000

    #1 Лаба Реализация комплексных чисел Уже реализовано: - перегрузка операторов:  +,-,*,/,=,==,!=,+=,*=,-=,/=,-(унарный минус) - оператор вывода в поток << -функция возведения в натуральную степень - функция arg и abs - геттеры и сеттеры для действительной и мнимой части
    Планируется:
    - Продумать возможность неявного преобразования из целого или дробного числа (float, double) и возможность арифм. операций с такими числами.
    - Добавить юнит-тесты и отобразить покрытие кода тестами.

---
+++ LAB 1/Complex.cpp
@@ -0,0 +1,36 @@
+//
+// Created by User on 11.10.2020.
+//
+#include "Complex.h"
+using namespace std;
+int main() {
+  Complex a(5, 2);
+  Complex b(3, -3);
+
+  cout << "a = " << a << "; b =" << b << endl;
+  if (a == b)
+    cout << 1 << endl;
+  else if (a != b)
+    cout << 2 << endl;
+
+  a += b;
+
+  cout << "a = " << a << "; b =" << b << endl;
+
+  Complex c = a - b;
+
+  cout << c << endl;
+  cout << a + b << endl;
+  cout << a * b << endl;
+  c = a / b;
+  cout << c << endl;
+  a /= b;
+  b = -b;
+  cout << a << b << endl;
+  a = b;
+  cout << a<<endl;
+  b=a^3;
+  cout<<a<<b<<endl;
+
+  return 0;
+}
---
+++ LAB 1/Complex.h
@@ -0,0 +1,172 @@
+//
+// Created by User on 11.10.2020.
+//
+#include <iostream>
+#include <cmath>
+
+#ifndef PMI_T_3_H_CPP_LAB_1_COMPLEX_H_
+#define PMI_T_3_H_CPP_LAB_1_COMPLEX_H_
+
+class Complex {
+ public:
+  Complex() = default;
+
+  [[maybe_unused]] explicit Complex(double valid) {
+    real = valid;
+  }
+
+  [[maybe_unused]] Complex(double valid, double alleged) {
+    real = valid;
+    imaginary = alleged;
+  }
+
+  Complex(const Complex &fresh) {
+    real = fresh.real;
+    imaginary = fresh.imaginary;
+  }
+
+  [[maybe_unused]] [[nodiscard]] double re() const { return real; }
+  [[maybe_unused]] [[nodiscard]] double im() const { return imaginary; }
+
+  // деструктор
+  ~Complex() {
+  }
+
+  // сложение
+  Complex operator+(const Complex &fresh) const {
+    return Complex(real + fresh.real, imaginary + fresh.imaginary);
+  }
+
+  // вычитание
+  Complex operator-(const Complex &fresh) const {
+    return Complex(real - fresh.real, imaginary - fresh.imaginary);
+  }
+
+  // умножение
+  Complex operator*(const Complex &fresh) const {
+    return Complex(real * fresh.real - imaginary * fresh.imaginary, real * fresh.imaginary + imaginary * fresh.real);
+  }
+
+  // деление
+  Complex operator/(const Complex &fresh) const {
+    Complex temp{};
+
+    double r = fresh.real * fresh.real + fresh.imaginary * fresh.imaginary;
+    temp.real = (real * fresh.real + imaginary * fresh.imaginary) / r;
+    temp.imaginary = (imaginary * fresh.real - real * fresh.imaginary) / r;
+
+    return temp;
+  }
+
+  // оператор =
+  Complex &operator=(const Complex &fresh) = default;
+
+  // оператор ==
+  bool operator==(const Complex &fresh) const {
+
+    if (this->real == fresh.real && this->imaginary == fresh.imaginary)
+      return true;
+    else
+      return false;
+  }
+
+  // оператор !=
+  bool operator!=(const Complex &fresh) const {
+    return !(*this == fresh);
+  }
+
+  // оператор +=
+  Complex &operator+=(const Complex &fresh) {
+    return *this = *this + fresh;
+  }
+
+  // оператор -=
+  Complex &operator-=(const Complex &fresh) {
+    return *this = *this - fresh;
+  }
+
+  // оператор *=
+  Complex &operator*=(const Complex &fresh) {
+    return *this = *this * fresh;
+  }
+
+  // оператор /=
+  Complex operator/=(const Complex &fresh) {
+    double re;
+    double r = fresh.real * fresh.real + fresh.imaginary * fresh.imaginary;
+    re = (this->real * fresh.real + this->imaginary * fresh.imaginary) / r;
+    this->imaginary = (this->imaginary * fresh.real - this->real * fresh.imaginary) / r;
+    this->real = re;
+    return *this;
+  }
+
+  // оператор -Complex
+  Complex operator-() const {
+    return Complex(-real, -imaginary);
+  }
+//  Complex& operator+=(double valid,double alleged) {
+//    return *this = *this + [valid,alleged];
+//  }
+//
+//  Complex &operator-=(double valid, double alleged) {
+//    return *this = *this - [valid, alleged];
+//  }
+//
+//  Complex &operator*=(double valid, double alleged) {
+//    return *this = *this * [valid, alleged];
+//  }
+
+  // Возведение в натуральную степень
+  Complex operator^(unsigned int num) {
+    Complex temp;
+    temp.real = this->real;
+    temp.imaginary = this->imaginary;
+    for (unsigned int i = 1; i < num; i++) {
+      temp=this->operator*(Complex(temp.real, temp.imaginary));
+    }
+    return temp;
+  }
+
+  // Модуль комплексного числа
+  [[nodiscard]] double abs() const {
+    return sqrt(real * real + imaginary * imaginary);
+  }
+
+  // оператор <<
+  friend std::ostream &operator<<(std::ostream &output, const Complex &fresh) {
+    if (fresh.real == 0 && fresh.imaginary == 0) {
+      output << 0;
+    } else if (fresh.real != 0 && fresh.imaginary == 0) {
+      output << fresh.real;
+    } else if (fresh.real == 0) {
+      if (fresh.imaginary == 1)
+        output << "i";
+      else if (fresh.imaginary == -1)
+        output << "-i";
+      else
+        output << fresh.imaginary << "i";
+    } else {
+      if (fresh.imaginary == 1)
+        output << "(" << fresh.real << "i)";
+      else if (fresh.imaginary == -1)
+        output << "(" << fresh.real << "-i)";
+      else if (fresh.imaginary > 0)
+        output << "(" << fresh.real << "+" << fresh.imaginary << "i)";
+      else
+        output << "(" << fresh.real << fresh.imaginary << "i)";
+    }
+
+    return output;
+  }
+
+  // оператор >>
+  friend std::istream &operator>>(std::istream &input, Complex &fresh) {
+    input >> fresh.real >> fresh.imaginary;
+    return input;
+  }
+
+ private:
+  double real = 0, imaginary = 0;  // действительная и мнимая части
+};
+
+#endif //PMI_T_3_H_CPP_LAB_1_COMPLEX_H_
