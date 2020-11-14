24eb688c1fb6dd3f3065a907c486766aa448f547
PrincePepper <semyon3336@gmail.com>
2020-11-14 22:44:45 +1000

    #fix linter

--- tournament 2/3_FixedAllocator/fixed_allocator.cpp
+++ tournament 2/3_FixedAllocator/fixed_allocator.cpp
@@ -3,19 +3,20 @@
 
 class PageAllocator {
   const std::uint64_t page_size_;
-  std::vector<void*> pages_;
+
+  std::vector<void *> pages_;
 
  public:
   PageAllocator(std::uint64_t page_size) : page_size_(page_size) {
   }
 
   ~PageAllocator() {
-    for (void* page : pages_)
+    for (void *page : pages_)
       ::operator delete(page);
   }
 
-  void* Allocate() {
-    void* page = ::operator new(page_size_);
+  void *Allocate() {
+    void *page = ::operator new(page_size_);
     pages_.push_back(page);
     return page;
   }
@@ -27,18 +28,18 @@ class PageAllocator {
 
 #include "fixed_allocator.h"
 
-int main()
-{
+int main() {
   srand(666);
 
-  const auto require_allocs = [](std::uint64_t expected, std::uint64_t actual) -> void {
-    if (expected == actual)
-      return;
-    std::cout << "Invalid count of page allocations" << std::endl;
-    std::cout << "Expected: " << expected << std::endl;
-    std::cout << "You have: " << actual << std::endl;
-    std::exit(0);
-  };
+  const auto require_allocs =
+      [](std::uint64_t expected, std::uint64_t actual) -> void {
+        if (expected == actual)
+          return;
+        std::cout << "Invalid count of page allocations" << std::endl;
+        std::cout << "Expected: " << expected << std::endl;
+        std::cout << "You have: " << actual << std::endl;
+        std::exit(0);
+      };
 
   {
     FixedAllocator<int> a(1);
@@ -57,7 +58,7 @@ int main()
       a.Allocate();
     require_allocs(1, a.InnerAllocator().Allocated());
   }
-
+ 
   {
     FixedAllocator<std::string> a(100);
     for (int i = 0; i < 50; ++i)
@@ -74,8 +75,7 @@ int main()
 
   {
     FixedAllocator<std::string> a(1);
-    for (int i = 0; i < 50; ++i)
-    {
+    for (int i = 0; i < 50; ++i) {
       a.Allocate();
       a.Deallocate(a.Allocate());
     }
@@ -84,8 +84,7 @@ int main()
 
   {
     FixedAllocator<std::string> a(100);
-    for (int i = 0; i < 500; ++i)
-    {
+    for (int i = 0; i < 500; ++i) {
       a.Allocate();
       a.Deallocate(a.Allocate());
     }
@@ -94,8 +93,7 @@ int main()
 
   {
     FixedAllocator<std::string> a(100);
-    for (int i = 0; i < 1000; ++i)
-    {
+    for (int i = 0; i < 1000; ++i) {
       a.Allocate();
       a.Deallocate(a.Allocate());
     }
@@ -104,8 +102,7 @@ int main()
 
   {
     FixedAllocator<std::string> a(1000000);
-    for (int i = 0; i < 1000000; ++i)
-    {
+    for (int i = 0; i < 1000000; ++i) {
       a.Allocate();
       a.Deallocate(a.Allocate());
     }
@@ -114,15 +111,13 @@ int main()
 
   {
     FixedAllocator<char> a(100);
-    std::vector<char*> store;
+    std::vector<char *> store;
     for (int i = 0; i < 100; ++i)
       store.push_back(a.Allocate());
-    for (int i = 0; i < 10000; ++i)
-    {
+    for (int i = 0; i < 10000; ++i) {
       const int rnd = rand() % 100;
       a.Deallocate(store[rnd]);
-      if (a.Allocate() != store[rnd])
-      {
+      if (a.Allocate() != store[rnd]) {
         std::cout << "Invalid allocation result" << std::endl;
         return 0;
       }
