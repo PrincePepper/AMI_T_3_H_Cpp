cf6990e6938d906771908c9d92b3eb0314bafddd
PrincePepper <semyon3336@gmail.com>
2020-12-14 02:58:34 +1000

    #3 контест - 1 решена

--- tournament 3/1_initialize_vector/initialize_vector.h
+++ tournament 3/1_initialize_vector/initialize_vector.h
@@ -1,21 +1,21 @@
 //
-// Created by User on 14.12.2020.
+// Created by Semen Sereda on 14.12.2020.
 //
 
 #ifndef PMI_T_3_H_CPP_TOURNAMENT_3_1_INITIALIZE_VECTOR_INITIALIZE_VECTOR_H_
 #define PMI_T_3_H_CPP_TOURNAMENT_3_1_INITIALIZE_VECTOR_INITIALIZE_VECTOR_H_
 #include <vector>
 
-template<typename T, typename V, typename D>
+template<typename V, typename D>
 auto initialize_vector(V v, D d) {
-  std::vector<T> vec(d, v);
+  std::vector<decltype(v)> vec(d, v);
   return vec;
 }
 
-template<typename T, typename V, typename D, typename... Args>
+template<typename V, typename D, typename... Args>
 auto initialize_vector(V v, D d, Args... args) {
-  std::vector<typeof(initialize_vector<T>(v, args...))>
-      vec(d, initialize_vector<T>(v, args...));
+  std::vector<decltype(initialize_vector(v, args...))>
+      vec(d, initialize_vector(v, args...));
   return vec;
 }
 
